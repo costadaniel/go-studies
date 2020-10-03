bc65e904a38eac2df172cc329a7daf0f104a5ffa
costadaniel <costadaniel@alu.ufc.br>
2020-10-03 19:45:44 -0300

    Finalização seção inicial

--- go-com-testes/1-OlaMundo/ola.go
+++ go-com-testes/1-OlaMundo/ola.go
@@ -2,11 +2,42 @@ package main
 
 import "fmt"
 
-// Ola retorna uma saudação no formato "Olá, <nome>!"
-func Ola(nome string) string {
-	return "Olá, " + nome + "!"
+const espanhol = "espanhol"
+const frances = "francês"
+const prefixoOlaPortuges = "Olá, "
+const prefixoOlaEspanhol = "Hola, "
+const prefixoOlaFrances = "Bonjour, "
+
+// Ola retorna uma saudação no formato "Olá, <nome>!" dependendo da linguagem
+func Ola(nome, idioma string) string {
+	if nome == "" {
+		nome = "Mundo"
+	}
+
+	return prefixoSaudacao(idioma) + nome
+}
+
+/*
+ *	Função privada... funções privadas começam com letras minusculas
+ *  enquanto publicas começam com letras maiusculas
+ */
+func prefixoSaudacao(idioma string) (prefixo string) {
+	switch idioma {
+	case espanhol:
+		prefixo = prefixoOlaEspanhol
+	case frances:
+		prefixo = prefixoOlaFrances
+	default:
+		prefixo = prefixoOlaPortuges
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Ola("Daniel"))
+	fmt.Println(Ola("Daniel", ""))
 }
+
+// Ciclo TDD:
+// Escreva um teste que falhe e veja-o falhar, para que saibamos que escrevemos um teste relevante para nossos requisitos e vimos que ele produz uma descrição da falha fácil de entender
+// Escrever a menor quantidade de código para fazer o teste passar, para que saibamos que temos software funcionando
+// Em seguida, refatorar, tendo a segurança de nossos testes para garantir que tenhamos um código bem feito e fácil de trabalhar
--- go-com-testes/1-OlaMundo/ola_test.go
+++ go-com-testes/1-OlaMundo/ola_test.go
@@ -3,10 +3,29 @@ package main
 import "testing"
 
 func TestOla(t *testing.T) {
-	resultado := Ola("Daniel")
-	esperado := "Olá, Daniel!"
-
-	if resultado != esperado {
-		t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
+	verificarMensagemCorreta := func(t *testing.T, resultado, esperado string) {
+		t.Helper()
+		if resultado != esperado {
+			t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
+		}
 	}
+
+	t.Run("Diz Olá para as pessoas, dado o idioma. Teste do idioma Espanhol", func(t *testing.T) {
+		resultado := Ola("Daniel", "espanhol")
+		esperado := "Hola, Daniel"
+		verificarMensagemCorreta(t, resultado, esperado)
+
+	})
+
+	t.Run("Diz Olá para as pessoas, dado o idioma. Teste do idioma Francês", func(t *testing.T) {
+		resultado := Ola("Daniel", "francês")
+		esperado := "Bonjour, Daniel"
+		verificarMensagemCorreta(t, resultado, esperado)
+	})
+
+	t.Run("Diz 'Olá, mundo' quando uma string vazia for passada nos campos", func(t *testing.T) {
+		resultado := Ola("", "")
+		esperado := "Olá, Mundo"
+		verificarMensagemCorreta(t, resultado, esperado)
+	})
 }
